af8af3509c1511bea30825c7fef2b03917dd6618
Shreyash Jadhav <34859507+shrejad@users.noreply.github.com>
2019-10-24 10:05:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 
+from bottle import route, run, default_app
+
+@route('/')
+def index():
+    return "<h1> hello OpenShift V3 Ninja</h1>"
+
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=8080)
+
+app = default_app()
